57c09734aba17e3aa8540b0f5b0efb3b0f0f0c97
yvromero <yvonne.romero@uds.edu.py>
2023-09-04 15:55:25 -0400

    Add NextUI to apply Grid

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -9,99 +9,109 @@ interface Props {
 }
 
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
-  return (
-    <Layout title='Tok Tok pokemon'>
-      <Grid.Container 
-        css={{ marginTop: '5px' }}
-        gap={2}
-      >
-        <Grid xs={12} sm={4}>
-          <Card hoverable css={{ padding: '30px' }}>
-          <Text h2 css={{ 
-                textTransform: 'capitalize',
-                fontFamily: '$mono',
-                textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-                textAlign: 'center'
-              }}>
-                {pokemon.name}
-              </Text>
-            <Card.Body>
-              <Card.Image
-                src={pokemon.sprites.other?.dream_world.front_default || '/no-image.png'}
-                alt={pokemon.name}
-                width="100%"
-                height={300}
-              />
-            </Card.Body>
-          </Card>
-        </Grid>
-        
-        <Grid xs={12} sm={8}>
-          <Card>
-            <Card.Header css={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
 
-              '@media (min-width: 40px)': {
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              },
-            }}>
-              <Text size={30} css={{
-                fontFamily: '$mono',
-                textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-              }}>
-                Sprites
-              </Text>
-              <Button
-                color="gradient"
-                ghost
-                css={{
-                  marginTop: '5px',
-                  marginLeft: 'auto',
-                  textTransform: 'none',
-                  fontFamily: '$mono',
-                }}>
-                Agregar a Favoritos
-              </Button>
-            </Card.Header>
+        return (
 
-            <Card.Body>
-              <Container css={{ display: 'flex', flexDirection: 'row' }}>
-                <Image
-                  src={pokemon.sprites.front_default}
-                  alt={pokemon.name}
-                  width={100}
-                  height={100}
-                />
-                <Image
-                  src={pokemon.sprites.back_default}
-                  alt={pokemon.name}
-                  width={100}
-                  height={100}
-                />
-                <Image
-                  src={pokemon.sprites.front_shiny}
-                  alt={pokemon.name}
-                  width={100}
-                  height={100}
-                />
-                <Image
-                  src={pokemon.sprites.back_shiny}
-                  alt={pokemon.name}
-                  width={100}
-                  height={100}
-                />
-              </Container>
-            </Card.Body>
-          </Card>
-        </Grid>
-      </Grid.Container>
-    </Layout>
-  )
+          <Layout title='Tok Tok pokemon'>
+
+            <Grid.Container css={{ marginTop: '5px' }} gap={ 2 }>
+              <Grid xs={ 12 } sm={ 4 } >
+                <Card hoverable css={{ padding: '30px' }}>
+
+                  <Text h2 css={{ 
+                    textTransform: 'capitalize',
+                    fontFamily: '$mono',
+                    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                    textAlign: 'center'
+                  }}>
+
+                    {pokemon.name}
+
+                  </Text>
+
+                    <Card.Body>
+                      <Card.Image 
+                        src={ pokemon.sprites.other?.dream_world.front_default || '/no-image.png' }
+                        alt={ pokemon.name }
+                        width="100%"
+                        height={ 200 }
+                      />
+                    </Card.Body>
+                </Card>
+              </Grid>
+
+              <Grid xs={ 12 } sm={ 8 }>
+                <Card>
+                  <Card.Header>
+                    <Text size={30} css={{
+                      fontFamily: '$mono',
+                      textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                    }}>
+                      Sprites
+                    </Text>
+
+                    <Button
+                      color="gradient"
+                      ghost
+                        css={{
+                          marginLeft: 'auto',
+                          textTransform: 'none',
+                          fontFamily: '$mono',
+                        }}>
+                          Guardar en favoritos
+                    </Button>
+                  </Card.Header>
+
+
+              <Card.Body>
+                <Container direction="row" gap={2} justify="center" wrap="wrap">
+                  <Grid.Container gap={2}>
+
+                    <Grid xs={12} sm={6} md={3}>
+                      <Image
+                        src={pokemon.sprites.front_default}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                      />
+                    </Grid>
+
+                    <Grid xs={12} sm={6} md={3}>
+                      <Image
+                        src={pokemon.sprites.back_default}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                      />
+                    </Grid>
+
+                    <Grid xs={12} sm={6} md={3}>
+                      <Image
+                        src={pokemon.sprites.front_shiny}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                      />
+                    </Grid>
+
+                    <Grid xs={12} sm={6} md={3}>
+                      <Image
+                        src={pokemon.sprites.back_shiny}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                        />
+                      </Grid>
+                    </Grid.Container>
+                  </Container>
+                </Card.Body>
+              </Card>
+            </Grid>
+          </Grid.Container>
+        </Layout>
+    )
 };
+
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
   const pokemons649 = [...Array(649)].map((value, index) => `${index + 1}`);
