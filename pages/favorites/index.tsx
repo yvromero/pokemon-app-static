ab90d191785011dce28033c929078593cb175457
yvromero <yvonne.romero@uds.edu.py>
2023-09-05 09:41:19 -0400

    Build favorites pokemons list

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Layout } from '@/components/layouts';
 import { localFavorites } from '@/utils';
 import { NoFavorites } from '@/components/ui';
+import { Grid, Card } from '@nextui-org/react';
 
 const FavoritesPage = () => {
 
@@ -15,9 +16,32 @@ const FavoritesPage = () => {
 
 
   return (
-    <Layout 
-      title='Mis Pokemones Favoritos>'>
-      <NoFavorites/>
+    <Layout title='Mis Pokemones Favoritos>'>
+
+      {
+        favoritePokemons.length === 0
+          ? ( <NoFavorites/> )
+          : ( 
+            <Grid.Container gap={ 2 } direction='row' justify='flex-start'>
+
+              {
+                favoritePokemons.map( id => (
+                  <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ id }>
+                    <Card hoverable clickable css={{ padding: 10 }}>
+                      <Card.Image
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ id }.svg`}
+                        width={'100%'}
+                        height={ 140 }
+                      />
+                    </Card>
+                  </Grid>
+                ))
+              }
+            </Grid.Container>
+          ) 
+      }
+
+
     </Layout>
   )
 };
